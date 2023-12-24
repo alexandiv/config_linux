d40de29b216199b8b5da661f2c97128a8ed8d35d
alexandiv <alexandiv@yandex.ru>
2023-12-24 18:40:24 +0300

    create script to end of arch install

---
+++ arch-install-end.sh
@@ -0,0 +1,32 @@
+# do it after arch-chroot
+pacman -S man-db man-pages texinfo
+pacman -S e2fsprogs dosfstools btrfs-progs exfat-utils 
+pacman -S networkmanager terminus-font openssh
+pacman -S rsync vim nano git cmake wget 
+pacman -S --needed base-devel
+
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >  /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "FONT=ter-v32n" >> /etc/vconsole.conf
+echo "archlinux" > /etc/hostname
+
+locale-gen
+systemctl enable NetworkManager
+mkinitcpio -P
+useradd -m -G wheel admin
+
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "!!!!!!!!!!!!!!"
+echo "passwd admin"
+echo "EDITOR=nano visudo"
+echo "/etc/hostname"
+
+
+
--- arch-kde.sh
+++ arch-kde.sh
@@ -1,8 +1,6 @@
 sudo pacman -S xorg
-sudo pacman -S plasma-meta plasma-wayland-session
-sudo pacman -S kde-system-meta
+sudo pacman -S plasma-meta plasma-wayland-session kde-system-meta
 sudo pacman -S kmag gwenview kcolorchooser kdegraphics-thumbnailers kolourpaint kruler okular spectacle svgpart
-sudo pacman -S dragon elisa ffmpegthumbs
-sudo pacman -S kio-extras kio-gdrive krdc krfb
-sudo pacman -S ark unrar dolphin-plugins kompare kcharselect kclock kcalc kfind konsole print-manager
+sudo pacman -S dragon elisa ffmpegthumbs kio-extras kio-gdrive krdc krfb
+sudo pacman -S ark unrar dolphin-plugins kompare kcharselect kclock kcalc kfind konsole kate print-manager
 
